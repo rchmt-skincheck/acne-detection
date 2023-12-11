5c69e3e23eead89c39d3a521c8432db324ca8a11
Dimas Tri Mustakim <test31122001@gmail.com>
2023-12-11 19:10:49 +0700

    update how to report the data

--- Detection.py
+++ Detection.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 import requests
 
+
 def detector(data, model):
     try:
         file_path = data["name"]
@@ -13,7 +14,7 @@ def detector(data, model):
     except Exception as e:
         print(f"Debug: {e}")
         raise Exception("Error decode file data")
-    
+
     try:
         # Read the input image using OpenCV
         req = urllib.request.urlopen(image_path)
@@ -22,29 +23,37 @@ def detector(data, model):
     except Exception as e:
         print(f"DEBUG: exception when trying to read image. Error message: {e}")
         raise Exception("Error when trying to read image")
-    
+
     try:
         model = YOLO(model)
-        res = model.predict(image, project="output", name=file_path, boxes=True, save=True, imgsz=600, conf=0.2, hide_labels=True)
-        
+        res = model.predict(
+            image,
+            project="output",
+            name=file_path,
+            boxes=True,
+            save=True,
+            imgsz=600,
+            conf=0.2,
+            hide_labels=True,
+        )
+
         # run inference on the source image
         results = model(image)
         # get the model names list
         names = model.names
         # get the 'acne' class id
-        acne_id = list(names)[list(names.values()).index('fore')]
+        acne_id = list(names)[list(names.values()).index("fore")]
         # count 'acne’ objects in the results
         count = results[0].boxes.cls.tolist().count(acne_id)
         print(count)
 
-
     except Exception as e:
         print(f"DEBUG: exception when trying to predict image. Error message: {e}")
         raise Exception("Error when trying to predict image")
-    
+
     try:
         post_response = post_request(
-            file_name=file_path,
+            file_name=f"{bucket_name}/{file_path}",
             image_result=f"output/{file_path}/image0.jpg",
             acne_count=count,
         )
@@ -52,20 +61,20 @@ def detector(data, model):
         print(f"DEBUG: exception when trying to post request. Error message: {e}")
         raise Exception("Error when trying to post request")
 
-def post_request(file_name, image_result, acne_count):
-    data = {
-        "ImageName": file_name,
-        "Count": acne_count,
-    }
 
+def post_request(file_name, image_result, acne_count):
     with open(image_result, "rb") as image_file:
-        files = {'ImageResult': (image_result, image_file, 'image/jpeg')}
+        files = {
+            "file": (image_result, image_file, "image/jpeg"),
+            "image_name": file_name,
+            "count": acne_count,
+        }
         response = requests.post(
-            "https://api-server-2p7ot3jdoq-et.a.run.app/internal/acne-detection",
-            files=files, data=data
+            "https://skincheckai.id/api/internal/v1/acne-detection",
+            file=files,
         )
 
     if response.status_code == 200:
         print("Permintaan berhasil!")
     else:
-        print("Gagal. Kode status:", response.status_code)
+        print("Gagal. Kode status: ", response.status_code, " , payload ")
