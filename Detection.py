628c623a4d40c12be03a70d8bc2d0b553971dc78
Rchmt-cmd <rachmath.059@gmail.com>
2024-01-06 13:16:20 +0800

    model update

--- Detection.py
+++ Detection.py
@@ -41,11 +41,22 @@ def detector(data, model):
         results = model(image)
         # get the model names list
         names = model.names
+
         # get the 'acne' class id
-        acne_id = list(names)[list(names.values()).index("fore")]
+        comedone_id = list(names)[list(names.values()).index("comedone")]
+        pustule_id = list(names)[list(names.values()).index("pustule")]
+        nodule_id = list(names)[list(names.values()).index("nodule")]
+        papule_id = list(names)[list(names.values()).index("papule")]
+
         # count 'acne’ objects in the results
-        count = results[0].boxes.cls.tolist().count(acne_id)
-        print(count)
+        comedone_count = results[0].boxes.cls.tolist().count(comedone_id)
+        pustule_count = results[0].boxes.cls.tolist().count(pustule_id)
+        nodule_count = results[0].boxes.cls.tolist().count(nodule_id)
+        papule_count = results[0].boxes.cls.tolist().count(papule_id)
+        print(f"comedone: {comedone_count}")
+        print(f"pustule: {pustule_count}")
+        print(f"nodule: {nodule_count}")
+        print(f"papule: {papule_count}")
 
     except Exception as e:
         print(f"DEBUG: exception when trying to predict image. Error message: {e}")
@@ -55,21 +66,29 @@ def detector(data, model):
         post_request(
             image_name=f"/{bucket_name}/{file_path}",
             image_result=f"output/{file_path}/image0.jpg",
-            acne_count=count,
+            comedone_count=comedone_count,
+            pustule_count=pustule_count,
+            nodule_count=nodule_count,
+            papule_count=papule_count,
         )
     except Exception as e:
         print(f"DEBUG: exception when trying to post request. Error message: {e}")
         raise Exception("Error when trying to post request")
 
 
-def post_request(image_name, image_result, acne_count):
+def post_request(image_name, image_result, comedone_count, pustule_count, nodule_count, papule_count):
     with open(image_result, "rb") as image_file:
         files = {
             "file": (image_result, image_file),
         }
         data = {
             "image_name": image_name,
-            "count": int(acne_count),
+            "count": {
+                "comedone": int(comedone_count),
+                "pustule": int(pustule_count),
+                "nodule": int(nodule_count),
+                "papule": int(papule_count)
+            },
         }
         response = requests.post(
             "https://skincheckai.id/api/internal/v1/acne-detection",
@@ -87,3 +106,5 @@ def post_request(image_name, image_result, acne_count):
             " , payload: ",
             response.json(),
         )
+
+detector("image.jpg", "best.pt")
